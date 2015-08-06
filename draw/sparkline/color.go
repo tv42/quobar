5705cccac22fb074f05d9a071278b8d14462d393
Tommi Virtanen <tv@eagain.net>
2015-08-06 13:17:29 -0700

    PickColor picks from multiple choices based on thresholds

---
+++ draw/sparkline/color.go
@@ -0,0 +1,43 @@
+package sparkline
+
+import (
+	"image/color"
+)
+
+// Threshold gives a color to use for values near Max.
+type Threshold struct {
+	Max   uint64
+	Color color.Color
+}
+
+// PickColor picks a color in based on the given thresholds.
+//
+// Thresholds are expected to be in decreasing order.
+//
+// If value is greater than or equal to the greatest Max, the color
+// from that threshold is returned.
+//
+// If value is less than the Max of the last threshold, the color from
+// that threshold is returned.
+//
+// Otherwise, the color will be proportionally in between the color of
+// the matching threshold and the next one, based on where value is
+// between the two Max values.
+func PickColor(thresholds []Threshold, value uint64) color.Color {
+	for idx, cur := range thresholds {
+		next := Threshold{Max: 0, Color: cur.Color}
+		if idx < len(thresholds)-1 {
+			next = thresholds[idx+1]
+		}
+		if value < next.Max {
+			continue
+		}
+		p := 1 - (float32(value-next.Max) / float32(cur.Max-next.Max))
+		return Gradient(cur.Color, next.Color, p)
+	}
+
+	// The slice is logically extended with an item with Max 0, no
+	// uint64 input can be less than 0. We can only reach here if
+	// there were no thresholds.
+	return color.Black
+}
---
+++ draw/sparkline/color_test.go
@@ -0,0 +1,60 @@
+package sparkline_test
+
+import (
+	"image/color"
+	"testing"
+
+	"github.com/tv42/quobar/draw/sparkline"
+)
+
+var (
+	white  = color.RGBA{R: 255, G: 255, B: 255, A: 255}
+	green  = color.RGBA{R: 0, G: 255, B: 0, A: 255}
+	yellow = color.RGBA{R: 255, G: 255, B: 0, A: 255}
+	red    = color.RGBA{R: 255, G: 0, B: 0, A: 255}
+)
+
+var trafficLights = []sparkline.Threshold{
+	{
+		Max:   1024,
+		Color: white,
+	},
+	{
+		Max:   768,
+		Color: green,
+	},
+	{
+		Max:   512,
+		Color: yellow,
+	},
+	{
+		Max:   256,
+		Color: red,
+	},
+}
+
+func TestPickColorKnown(t *testing.T) {
+	for idx, test := range []struct {
+		input  uint64
+		expect color.Color
+	}{
+		{9000, white},
+		{100, red},
+		{0, red},
+		{1024, white},
+		{768, green},
+		{512, yellow},
+		{256, red},
+	} {
+		// if it goes through mixing, it's converted to RGBA64
+		if g, e := sparkline.PickColor(trafficLights, test.input), color.RGBA64Model.Convert(test.expect); g != e {
+			t.Errorf("#%d: value %d wrong color: %v != %v", idx, test.input, g, e)
+		}
+	}
+}
+
+func TestPickColorEmpty(t *testing.T) {
+	if g, e := sparkline.PickColor(nil, 42), color.Black; g != e {
+		t.Errorf("wrong color for empty slice: %v != %v", g, e)
+	}
+}
