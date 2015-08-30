0659f7a0a5da6343ce3a3e28117436bdd555b707
Tommi Virtanen <tv@eagain.net>
2015-08-29 19:48:54 -0700

    Use external "perceptualdiff" command for image approval
    Our sparkline rendering seems to have probabilistic antialiasing.

--- internal/approve/image.go
+++ internal/approve/image.go
@@ -8,6 +8,7 @@ import (
 	"image/color"
 	"image/png"
 	"os"
+	"os/exec"
 	"path"
 	"runtime"
 	"strings"
@@ -70,6 +71,23 @@ func Image(img image.Image) error {
 		return fmt.Errorf("size mismatch: %v != %v", g, e)
 	}
 
+	if err := strictEq(img, good); err == nil {
+		return nil
+	}
+	// try a fuzzy match
+	//
+	// If you know of a Go library implementing a perceptual diff (not
+	// just a perceptual hash), please tell me!
+	cmd := exec.Command("perceptualdiff", newName, goodName)
+	if buf, err := cmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("perceptualdiff failed:\n%s%s", buf, err)
+	}
+	return nil
+}
+
+// strictEq is a strict pixel by pixel comparison. It assumes the
+// images are the same size.
+func strictEq(img, good image.Image) error {
 	off := img.Bounds().Min.Sub(good.Bounds().Min)
 	for y := good.Bounds().Min.Y; y < good.Bounds().Max.Y; y++ {
 		for x := good.Bounds().Min.X; x < good.Bounds().Max.X; x++ {
@@ -77,7 +95,7 @@ func Image(img image.Image) error {
 			p := image.Point{X: x, Y: y}.Add(off)
 			colorGot := img.At(p.X, p.Y)
 			if !colorEq(colorGood, colorGot) {
-				return fmt.Errorf("pixel difference at %v", p)
+				return fmt.Errorf("pixel difference at %v: %v != %v", p, colorGot, colorGood)
 			}
 		}
 	}
